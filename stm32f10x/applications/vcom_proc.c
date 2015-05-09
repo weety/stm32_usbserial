c3ffb4321366edc5e3c44031d15fe46e2ed97783
weety <luohui2320@gmail.com>
2015-05-09 16:50:24 +0800

    Used uart1 communicate to external device. Change finsh shell to uart2.

--- stm32f10x/applications/vcom_proc.c
+++ stm32f10x/applications/vcom_proc.c
@@ -1,11 +1,61 @@
 
 #include <board.h>
 #include <rtthread.h>
+#include <rtdevice.h>
 #include "vcom_proc.h"
 
 #define VCOM_BUFSZ 2048
 static rt_uint32_t vcom_buffer[VCOM_BUFSZ/4];
 static struct rt_semaphore vcom_sem;
+static rt_device_t vcom_dev;
+
+#define BUFSZ 2048
+rt_uint32_t uart1_buffer[BUFSZ / 4];
+static struct rt_semaphore uart1_sem;
+static rt_device_t uart1_dev;
+
+rt_err_t uart1_rx_ind(rt_device_t dev, rt_size_t size)
+{
+	rt_sem_release(&uart1_sem);
+
+	return RT_EOK;
+}
+
+void uart1_thread_entry(void* parameter)
+{
+	rt_err_t err;
+	rt_size_t size = 0;
+	char *buf;
+	int count = 4;
+	int offset = 0;
+	
+	buf = (char *)uart1_buffer;
+
+	rt_device_set_rx_indicate(uart1_dev, uart1_rx_ind);
+
+	while(1)
+	{
+		err = rt_sem_take(&uart1_sem, RT_WAITING_FOREVER);
+		if (err != RT_EOK)
+		{
+			rt_kprintf("wait sem uart2 error\n");
+			break;
+		}
+
+		size = rt_device_read(uart1_dev, 0, buf, 1);
+		if (size > 0)
+		{
+			rt_device_write(vcom_dev, 0, buf, size);
+		}
+
+	}
+
+	rt_device_set_rx_indicate(uart1_dev, RT_NULL);
+	rt_device_close(uart1_dev);
+	rt_sem_detach(&uart1_sem);
+}
+
+
 rt_err_t vcom_rx_ind(rt_device_t dev, rt_size_t size)
 {
 	rt_sem_release(&vcom_sem);
@@ -18,11 +68,67 @@ void vcom_thread_entry(void* parameter)
 	rt_err_t err;
 	rt_size_t size = 0;
 	rt_uint8_t *buf;
-	rt_device_t vcom_dev;
 	int count = 4;
 	int offset = 0;
 
 	buf = (rt_uint8_t *)vcom_buffer;
+
+	rt_device_set_rx_indicate(vcom_dev, vcom_rx_ind);
+
+	while(1)
+	{
+		err = rt_sem_take(&vcom_sem, RT_WAITING_FOREVER);
+		if (err != RT_EOK)
+		{
+			rt_kprintf("wait sem vcom error\n");
+			break;
+		}
+
+		size = rt_device_read(vcom_dev, 0, buf, VCOM_BUFSZ);
+		if (size > 0)
+		{
+			rt_device_write(uart1_dev, 0, buf, size);
+		}
+
+	}
+
+	rt_device_set_rx_indicate(vcom_dev, RT_NULL);
+	rt_device_close(vcom_dev);
+	rt_sem_detach(&vcom_sem);
+}
+
+
+void vcom_init(void)
+{
+	rt_err_t err;
+	struct serial_configure config;
+	rt_thread_t uart1_thread;
+	rt_thread_t vcom_thread;
+
+	rt_sem_init(&uart1_sem, "uart1", 0, RT_IPC_FLAG_FIFO);
+
+	uart1_dev = rt_device_find("uart1");
+
+	if (!uart1_dev)
+	{
+		rt_kprintf("open device uart1 failed\n");
+	}
+
+	err = rt_device_open(uart1_dev, RT_DEVICE_OFLAG_RDWR);
+	if (err != RT_EOK)
+	{
+		rt_kprintf("open uart1 failed\n");
+		return;
+	}
+
+	config.baud_rate = 115200;
+    config.bit_order = BIT_ORDER_LSB;
+    config.data_bits = DATA_BITS_8;
+    config.parity    = PARITY_NONE;
+    config.stop_bits = STOP_BITS_1;
+    config.invert    = NRZ_NORMAL;
+
+	rt_device_control(uart1_dev, RT_DEVICE_CTRL_CONFIG, &config);
 
 	rt_sem_init(&vcom_sem, "vcom", 0, RT_IPC_FLAG_FIFO);
 	vcom_dev = rt_device_find("vcom");
@@ -39,35 +145,13 @@ void vcom_thread_entry(void* parameter)
 		return;
 	}
 
-	rt_device_set_rx_indicate(vcom_dev, vcom_rx_ind);
+	uart1_thread = rt_thread_create("uart1",
+								uart1_thread_entry, RT_NULL,
+								512, 15, 20);
 
-	while(1)
-	{
-		err = rt_sem_take(&vcom_sem, RT_WAITING_FOREVER);
-		if (err != RT_EOK)
-		{
-			rt_kprintf("wait sem vcom error\n");
-			break;
-		}
-
-		size = rt_device_read(vcom_dev, 0, buf, VCOM_BUFSZ);
-		if (size > 0)
-		{
-			rt_device_write(vcom_dev, 0, buf, size);
-		}
-
-	}
-
-	rt_device_set_rx_indicate(vcom_dev, RT_NULL);
-	rt_device_close(vcom_dev);
-	rt_sem_detach(&vcom_sem);
-}
-
-
-void vcom_init(void)
-{
+	if (uart1_thread != RT_NULL)
+		rt_thread_startup(uart1_thread);
 	
-	rt_thread_t vcom_thread;
 
 	vcom_thread = rt_thread_create("vcom",
 								vcom_thread_entry, RT_NULL,
--- stm32f10x/drivers/board.h
+++ stm32f10x/drivers/board.h
@@ -40,7 +40,7 @@
 
 // <o> Console on USART: <0=> no console <1=>USART 1 <2=>USART 2 <3=> USART 3
 // 	<i>Default: 1
-#define STM32_CONSOLE_USART		1
+#define STM32_CONSOLE_USART		2
 
 // <o> Ethernet Interface: <0=> Microchip ENC28J60 <1=> Davicom DM9000A
 // 	<i>Default: 0
